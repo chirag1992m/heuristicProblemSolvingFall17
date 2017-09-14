b0f81cc54587ec4c752e0432df0a625b7cad13cd
Chirag Maheshwari <chirag1992m.cc@gmail.com>
2017-09-14 14:50:24 -0400

    Added working bot for Expanding Nim based on the deterministic policy

---
+++ week1-Expanding_Nim/expandingNimBot/bot.py
@@ -0,0 +1,29 @@
+import pickle
+import os
+from table_1 import main as table_creator
+
+class Bot(object):
+
+    def __init__(self):
+        if not os.path.isfile("moves_table.pkl"):
+            table_creator()
+
+        self.table = pickle.load(open("moves_table.pkl", "rb"))
+        self.self_resets = 0
+        self.other_resets = 0
+
+        print("Bot Loaded!")
+
+    def set_resets(self, init_resets):
+        self.self_resets = init_resets
+        self.other_resets = init_resets
+
+    def get_move(self, stones_left, current_max, is_reset):
+        move = self.table[stones_left][current_max][self.self_resets][self.other_resets][1 if is_reset else 0]
+
+        if is_reset:
+            self.other_resets -= 1
+        if move[2]:
+            self.self_resets -= 1
+
+        return move[1], move[2]
---
+++ week1-Expanding_Nim/expandingNimBot/player.py
@@ -0,0 +1,55 @@
+import atexit
+import argparse
+from client import Client
+from bot import Bot
+
+def parse_arguments():
+    parser = argparse.ArgumentParser(description='Expanding Nim Player')
+    parser.add_argument('--n', dest="name", type=str, default='Player',
+                        help='Name of the player')
+    parser.add_argument('--f', dest="first", action="store_true",
+                        help="Is it the first player?")
+    parser.add_argument('--ip', type=str,
+                        help='IP Address of the server', default='127.0.0.1')
+    parser.add_argument('--port', type=int,
+                        help='Port of the server', default=9000)
+
+    return parser.parse_args()
+
+def check_game_status(game_state):
+    if game_state['finished']:
+        print(game_state['reason'])
+        exit(0)
+
+def parse_game_stats(game_stat):
+    stones = game_state['stones_left']
+    current_max = game_state['current_max']
+    reset_used = game_state['reset_used']
+
+    return stones, current_max, reset_used
+
+def default_game_stats(client):
+    return client.init_stones, 3, False
+
+if __name__ == '__main__':
+    args = parse_arguments()
+
+    print(args)
+
+    bot = Bot()
+
+    client = Client(args.name, args.first, (args.ip, args.port))
+    atexit.register(client.close)
+
+    bot.set_resets(client.init_resets)
+
+    if args.first:
+        stones, current_max, reset_used = default_game_stats(client)
+        num_stones, reset = bot.get_move(stones, current_max, reset_used)
+        check_game_status(client.make_move(num_stones, reset))
+    while True:
+        game_state = client.receive_move()
+        check_game_status(game_state)
+        stones, current_max, reset_used = parse_game_stats(game_state)
+        num_stones, reset = bot.get_move(stones, current_max, reset_used)
+        check_game_status(client.make_move(num_stones, reset))
