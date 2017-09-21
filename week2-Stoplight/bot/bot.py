097d361e156aebd829a9a053abaa95ddf9c70364
Chirag Maheshwari <chirag1992m.cc@gmail.com>
2017-09-21 16:41:27 -0400

    Fixing some errors in the bot for Stoplight game

--- week2-Stoplight/bot/bot.py
+++ week2-Stoplight/bot/bot.py
@@ -1,3 +1,5 @@
+from __future__ import print_function
+
 import Queue
 import sys
 
@@ -43,17 +45,19 @@ def read_stoplight_info_file(file_lines):
 
     return node_list, edge_list, color_list, start_node, end_node
 
-def construct_graph(node_list, edge_list):
+
+def construct_graph(node_list, edge_list, color_list):
     graph = dict()
     for i, v in enumerate(node_list):
         graph[v] = []
 
     for i, v in enumerate(edge_list):
-        graph[v[0]].append((v[1], v[2], v[3]))
-        graph[v[1]].append((v[0], v[2], v[3]))
+        if v[3] <= color_list[v[2]][0]:
+            graph[v[0]].append((v[1], v[2], v[3]))
+            graph[v[1]].append((v[0], v[2], v[3]))
 
-    print "Graph Constructed"
     return graph
+
 
 def distance_to_neighbor(current_time, neighbor, color_list):
     green_time, red_time = color_list[neighbor[1]]
@@ -65,28 +69,61 @@ def distance_to_neighbor(current_time, neighbor, color_list):
         return green_time + red_time - time_passed + neighbor[2]
 
 
+def get_path(path_from_start, start_node, end_node):
+    path = [(end_node, ('', '', ''))]
+
+    current_node = end_node
+    while current_node != start_node:
+        current_node, edge = path_from_start[current_node][1], path_from_start[current_node][2]
+        path.append((current_node, edge))
+
+    return path[-1::-1]
+
+
+def get_moves(full_path, color_list):
+    current_time = 0
+    moves = []
+
+    for i in range(len(full_path) - 1):
+        start_node, end_node = full_path[i], full_path[i+1]
+        end_time = distance_to_neighbor(current_time, start_node[1], color_list)
+        start_time = end_time - start_node[1][2]
+
+        move = [start_node[0], end_node[0], str(start_time), str(end_time)]
+        moves.append(' '.join(move))
+
+    return '\n'.join(moves)
+
+
 def stoplight_dijkstra(node_list, edge_list, color_list, start_node, end_node):
-    graph = construct_graph(node_list, edge_list)
+    graph = construct_graph(node_list, edge_list, color_list)
 
     path_from_start = dict()
     for i, v in enumerate(node_list):
-        path_from_start[v] = (sys.maxint, '')
-    path_from_start[start_node] = (0, start_node)
+        path_from_start[v] = (sys.maxint, ())
+    path_from_start[start_node] = (0, start_node, '')
 
-    queue = Queue.PriorityQueue(maxsize=len(node_list))
+    queue = Queue.PriorityQueue()
     queue.put((0, start_node))
 
-    current_time = 0
+    visited = set()
+
     while not queue.empty():
         next_vertex = queue.get()
-        print next_vertex
+        if next_vertex[1] in visited:
+            continue
+
+        visited.add(next_vertex[1])
 
         current_time = next_vertex[0]
         for neighbor in graph[next_vertex[1]]:
             dist = distance_to_neighbor(current_time, neighbor, color_list) + current_time
 
-            if  dist < path_from_start[neighbor[0]][0]:
-                path_from_start[neighbor[0]] = (dist, next_vertex[1])
+            if dist < path_from_start[neighbor[0]][0]:
+                path_from_start[neighbor[0]] = (dist, next_vertex[1], neighbor)
                 queue.put((dist, neighbor[0]))
 
-    print path_from_start
+    full_path = get_path(path_from_start, start_node, end_node)
+    moves = get_moves(full_path, color_list)
+
+    return moves
