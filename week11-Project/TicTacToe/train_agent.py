a81bce0ca820aa1d64b34143c9ed69fe3dcc8507
Chirag Maheshwari <chirag1992m.cc@gmail.com>
2017-12-12 01:32:03 -0500

    Trying to get better than the random bot

--- week11-Project/TicTacToe/train_agent.py
+++ week11-Project/TicTacToe/train_agent.py
@@ -52,6 +52,7 @@ win_ratio_factor = .8
 win_ratio_min = 10
 early_stopping_patience = 50
 randomize_eval = True
+best_random_ratio = 0.0
 
 # Set Seeds for Reproducibility
 torch.manual_seed(42)
@@ -164,7 +165,7 @@ def evaluate(agent_1, agent_2):
     else:
         ratio = float('inf')
     print("Evaluation complete with win ratio: {}".format(ratio))
-    return ratio > win_ratio
+    return ratio > win_ratio, ratio
 
 
 TRAIN_DIR = 'tic_tac_toe_train_data'
@@ -189,16 +190,23 @@ for episode in range(1, episodes+1):
     train_model(training_data_paths, CURRENT_MODEL_PATH)
 
     current_agent = SelfPlayRLAgent(CURRENT_MODEL_PATH, "current_agent")
-    better = evaluate(current_agent, best_agent)
+    better, win_ratio_check = evaluate(current_agent, best_agent)
     print("Completed episode: {}".format(episode))
 
-    _ = evaluate(current_agent, RandomPlayer())
+    _, random_win_ratio = evaluate(current_agent, RandomPlayer())
     if better:
         improve_patience = 0
         win_ratio = max(win_ratio * win_ratio_factor, win_ratio_min)
-        if win_ratio == win_ratio_min:
-            print("Switching on evaluation mode...")
-            randomize_eval = False
+        print("Found better agent...updating...")
+        best_model.load_state_dict(current_model.state_dict())
+        os.remove(BEST_MODEL_PATH)
+        torch.save({'model': best_model.state_dict()}, BEST_MODEL_PATH)
+    elif best_random_ratio < random_win_ratio and (win_ratio_check == float('inf')
+                                                   or win_ratio_check == 0):
+        print("Updating for random bot improvement")
+        best_random_ratio = random_win_ratio
+        improve_patience = 0
+        win_ratio = max(win_ratio * win_ratio_factor, win_ratio_min)
         print("Found better agent...updating...")
         best_model.load_state_dict(current_model.state_dict())
         os.remove(BEST_MODEL_PATH)
@@ -208,3 +216,6 @@ for episode in range(1, episodes+1):
         if improve_patience >= improvement_patience:
             print("No improvement since last episodes. Stopping training...")
             break
+    if randomize_eval and win_ratio == win_ratio_min:
+        print("Switching on evaluation mode...")
+        randomize_eval = False
