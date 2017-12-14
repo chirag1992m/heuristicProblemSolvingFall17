Fixing probability normalization calculation

week11-Project/TicTacToe/model.py
@@ -31,10 +31,11 @@ class AlphaZeroTicTacToe(nn.Module):
 
         P = self.P_squash(self.P(intermediate))
         P = P * mask
-        P = P / torch.sum(P)
+        y = torch.sum(P, dim=2).clamp(min=1e-12)
+        y = torch.unsqueeze(y, dim=1)
+        P = P / y
 
         V = self.V_squash(self.V(intermediate))
-
         return P, V
 
 